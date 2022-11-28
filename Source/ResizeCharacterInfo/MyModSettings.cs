312ac71771cecb6c8c3e8389eadb5cd83bf5c35d
nonnonstop <42905588+nonnonstop@users.noreply.github.com>
2022-11-28 23:21:26 +0900

    Added needs tab setting

--- Source/ResizeCharacterInfo/MyModSettings.cs
+++ Source/ResizeCharacterInfo/MyModSettings.cs
@@ -7,10 +7,13 @@ namespace ResizeCharacterInfo
     {
         public float bioHeightOffset = 0f;
         public string bioHeightOffsetBuf;
+        public float needsHeightOffset = 0f;
+        public string needsHeightOffsetBuf;
 
         public override void ExposeData()
         {
             Scribe_Values.Look(ref bioHeightOffset, "bioHeightOffset", 0f);
+            Scribe_Values.Look(ref needsHeightOffset, "needsHeightOffset", 0f);
             base.ExposeData();
         }
 
@@ -21,6 +24,9 @@ namespace ResizeCharacterInfo
             listingStandard.TextFieldNumericLabeled(
                 "ResizeCharacterInfo.BioHeightOffset".Translate(),
                 ref bioHeightOffset, ref bioHeightOffsetBuf, 0f, 7680f);
+            listingStandard.TextFieldNumericLabeled(
+                "ResizeCharacterInfo.NeedsHeightOffset".Translate(),
+                ref needsHeightOffset, ref needsHeightOffsetBuf, 0f, 7680f);
             listingStandard.End();
         }
     }
---
+++ Source/ResizeCharacterInfo/NeedsCardUtilityPatch.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using HarmonyLib;
+using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace ResizeCharacterInfo
+{
+    [HarmonyPatch]
+    public class NeedsCardUtilityPatch
+    {
+        [HarmonyTranspiler]
+        [HarmonyPatch(typeof(NeedsCardUtility), "GetSize")]
+        public static IEnumerable<CodeInstruction> GetSizeTranspiler(IEnumerable<CodeInstruction> instructions)
+        {
+            var field = typeof(NeedsCardUtility).GetField("FullSize");
+            var found = false;
+
+            foreach (var instruction in instructions)
+            {
+                if (instruction.LoadsField(field))
+                {
+                    found = true;
+                    yield return instruction;
+                    yield return CodeInstruction.Call(typeof(NeedsCardUtilityPatch), "GetSize");
+                }
+                else
+                {
+                    yield return instruction;
+                }
+            }
+
+            if (!found)
+                Log.Error("[ResizeCharacterInfo] Cannot find target instruction (DrawCharacterCard)");
+        }
+
+        public static Vector2 GetSize(Vector2 value)
+        {
+            var settings = LoadedModManager.GetMod<MyMod>().GetSettings<MyModSettings>();
+            value.y += settings.needsHeightOffset;
+            return value;
+        }
+    }
+}
